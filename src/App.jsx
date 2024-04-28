04bf4d76e6a152ab1aa926d8bc7dcf0358a5f8d2
chenyj <chenyj@chenyj.lan>
2024-04-27 23:30:24 -0500

    Welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import viteLogo from '/vite.svg'
-// import './App.css'
+import "./App.css"
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -12,13 +12,15 @@ import {
 
 function App({ signOut }) {
   return (
+    <div slot="confirm-sign-up" style={{ textAlign: 'center' }}>
     <View className="App">
       <Card>
         <Image src={viteLogo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
+        <Heading level={1}>Welcome to React application using AWS Amplify!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
+    </div>
   );
 }
 
